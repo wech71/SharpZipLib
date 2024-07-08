4e62ef20f6e48ffc292fcd6595cc1bf86ce8ceb6
Christoph Weber <wech71@gmx.net>
2024-07-08 10:43:35 +0200

    SharZipLib test program (c#) to read zip4j file, created by zip4j test program "zip4jtest"

---
+++ samples/TestReadZip4JStoredEntriesZip/Program.cs
@@ -0,0 +1,32 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.IO;
+
+namespace TestReadZip4JStoredEntriesZip
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ReadZip4jTestZip();
+		}
+
+		private static void ReadZip4jTestZip()
+		{
+			using (ZipInputStream stm = new ZipInputStream(new FileStream(@"..\..\..\..\zip4jtest\zip4j-stored-entries.zip", FileMode.Open)))
+			{
+				while (true)
+				{
+					ZipEntry entry = stm.GetNextEntry();
+					if (entry == null)
+						break;
+
+					byte[] buf = new byte[entry.Size];
+					int bytesRead = stm.Read(buf, 0, buf.Length);
+					Console.WriteLine($"{entry.Name} read {bytesRead} bytes");
+				}
+			}
+
+		}
+	}
+}
